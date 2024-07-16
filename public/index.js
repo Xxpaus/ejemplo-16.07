b4c212fa524418d5ba770baa75487f820f0681d3
xXPausXx <castanogomezpaola@gmail.com>
2024-07-16 17:16:01 -0500

    hola

---
+++ public/index.js
@@ -0,0 +1,122 @@
+const formulario = document.querySelector('.aggUsuario');
+
+
+const agregaLocalStorage = (usuario) => {
+    try {
+        if (!localStorage.getItem('usuario')) {
+            localStorage.setItem('usuario', JSON.stringify([usuario]));
+            return;
+        }
+        const usuarios = obtenerLocalStorage();
+        usuarios.push(usuario);
+        localStorage.setItem('usuario', JSON.stringify(usuarios));
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+const obtenerLocalStorage = () => {
+    try {
+        return [...JSON.parse(localStorage.getItem('usuario'))];
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+const Usuarios = () => {
+    return {
+        nombre: '',
+        apellido: '',
+        email: '',
+        password: '',
+        url: '',
+        button :'',
+        
+
+        Usuario: (nombre, apellido, email, password, url) => {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.email = email;
+            this.password = password;
+            this.url = url;
+            this.button;
+        },
+
+        getNombre: () => {
+            return this.nombre;
+        },
+
+        jsonToHtml: (estilos=null) => {
+            const tarjeta = document.createElement('div');
+            const img = document.createElement('img');
+            const h5 = document.createElement('h5');
+            const p = document.createElement('p');
+            const button = document.createElement('button');
+            const button1 = document.createElement('button');
+        
+
+            if(estilos){
+                tarjeta.className = estilos;
+            }
+
+            if(this.url == '' ){
+                img.src = 'https://static.vecteezy.com/system/resources/thumbnails/000/550/980/small/user_icon_001.jpg';
+            } else {
+                img.src = this.url;
+            }
+
+            h5.textContent = `${this.nombre} ${this.apellido}`;
+            p.textContent = this.email;
+            button.textContent = "Editar"
+            button1.textContent = "Eliminar"
+
+            tarjeta.appendChild(img)
+            tarjeta.appendChild(h5)
+            tarjeta.appendChild(p)
+            tarjeta.appendChild(button)
+            tarjeta.appendChild(button1)
+
+            return tarjeta;
+        },
+
+        getUsuario: () => {
+            return {
+                nombre: this.nombre,
+                apellido: this.apellido,
+                email: this.email,
+                password: this.password,
+                url: this.url,
+                button: this.button,
+            }
+
+        },
+    }
+}
+
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const usuario = Usuarios();
+    const nombre = document.querySelector('#nombre').value;
+    const apellido = document.querySelector('#apellido').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const img = document.querySelector('#img').value;
+    
+
+
+
+    const section = document.querySelector('.usuarios');
+
+    usuario.Usuario(
+        nombre,
+        apellido,
+        email,
+        password,
+        img
+    );
+
+    section.appendChild(usuario.jsonToHtml())
+    
+});
